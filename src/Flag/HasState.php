16a38bf918f35d91d0c6a15fddaea67772bb35ae
Zan Baldwin <hello@zanbaldwin.com>
2024-01-19 15:01:14 +0100

    Add Rule for Class Statefulness

---
+++ src/Flag/HasState.php
@@ -0,0 +1,8 @@
+<?php declare(strict_types=1);
+
+namespace WeDevelop\PHPStan\ClassFlags\Flag;
+
+#[\Attribute(\Attribute::TARGET_CLASS)]
+final readonly class HasState
+{
+}
---
+++ src/Rule/IsStatefulRule.php
@@ -0,0 +1,56 @@
+<?php declare(strict_types=1);
+
+namespace WeDevelop\PHPStan\ClassFlags\Rule;
+
+use PhpParser\Node;
+use PHPStan\Analyser\Scope;
+use PHPStan\Node\InClassNode;
+use PHPStan\Rules\Rule;
+use PHPStan\Rules\RuleError;
+use PHPStan\Rules\RuleErrorBuilder;
+use WeDevelop\PHPStan\ClassFlags\Flag\HasState;
+use WeDevelop\PHPStan\ClassFlags\Util\AttributeHelper;
+
+/** @implements Rule<InClassNode> */
+final readonly class IsStatefulRule implements Rule
+{
+    public function getNodeType(): string
+    {
+        return InClassNode::class;
+    }
+
+    /**
+     * @param InClassNode $node
+     * @return RuleError[]
+     */
+    public function processNode(Node $node, Scope $scope): array
+    {
+        if (null === $classReflection = $scope->getClassReflection()?->getNativeReflection()) {
+            // Why were we passed a node that is not a class?
+            return [];
+        }
+
+        // Don't bother with adding #[HasState] to anonymous classes because it'll most likely confuse: are we adding
+        // the attribute to the anonymous class or the variable we are assigning it to?
+        // Also, don't add it to abstract classes - we only care about concrete implementations.
+        if ($classReflection->isAnonymous() || $classReflection->isAbstract()) {
+            return [];
+        }
+
+        $isReadonly = $classReflection->isReadOnly();
+        $isStateful = AttributeHelper::hasAttributeOnClass($classReflection, [HasState::class, \HasState::class]);
+
+        if ($isStateful == $isReadonly) {
+            $error = RuleErrorBuilder::message(sprintf('The statefulness of class "%s" is ambiguous.', $classReflection->getShortName(),))
+                ->file($scope->getFile())
+                ->line($node->getStartLine());
+            $isReadonly
+                ? $error->addTip(sprintf('Remove one of either readonly keyword or the "#[%s]" attribute.', \HasState::class))
+                : $error->addTip(sprintf('Explicitly mark it as readonly or add the "#[%s]" attribute.', \HasState::class));
+
+            return [$error->build()];
+        }
+
+        return [];
+    }
+}
